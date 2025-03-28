a9998c3c3379c1fc309f45a771c26390210a7880
Emmanuel-hub-bit <emmanuelonyango467@gmail.com>
2025-03-28 10:14:13 +0300

    navbar edits

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -7,16 +7,18 @@ const InfoBoxes = ({
     buttonInfo,
     children }) => {
     return (
-        <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
-            <h2 className={`${textColor} text-2xl font-bold`}>{ heading }</h2>
-            <p className={`${textColor} mt-2 mb-4`}>{ children }</p>
-            <Link
-              href={buttonInfo.link}
-              className={`${buttonInfo} inline-block text-white rounded-lg px-4 py-2 hover:bg-gray-700`}
-            >
-              { buttonInfo.text}
-            </Link>
-          </div>
+        <>
+            <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
+                <h2 className={`${textColor} text-2xl font-bold`}>{ heading }</h2>
+                <p className={`${textColor} mt-2 mb-4`}>{ children }</p>
+                <Link
+                href={buttonInfo.link}
+                className={`${buttonInfo.backgroundColor} inline-block text-white rounded-lg px-4 py-2 hover:bg-gray-700`}
+                >
+                { buttonInfo.text}
+                </Link>
+            </div>
+        </>
     );
 }
  
