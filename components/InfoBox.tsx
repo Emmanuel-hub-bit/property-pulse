f834f765e3c39adb43cd5cb2b50ad5b92b045921
Emmanuel-hub-bit <emmanuelonyango467@gmail.com>
2025-03-28 09:48:32 +0300

    navbar edits

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -4,16 +4,17 @@ const InfoBoxes = ({
     heading, 
     backgroundColor = "bg-gray-100", 
     textColor = 'text-gray-800',
+    buttonInfo,
     children }) => {
     return (
         <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
             <h2 className={`${textColor} text-2xl font-bold`}>{ heading }</h2>
             <p className={`${textColor} mt-2 mb-4`}>{ children }</p>
             <Link
-              href="/properties"
-              className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+              href={buttonInfo.link}
+              className={`${buttonInfo} inline-block text-white rounded-lg px-4 py-2 hover:bg-gray-700`}
             >
-              Browse Properties
+              { buttonInfo.text}
             </Link>
           </div>
     );
