6c53c27f5d4a6896ed05874ee6d7b4927ddd73dc
Emmanuel <emmanuelonyango467@gmail.com>
2025-04-10 14:06:27 +0300

    adding properties to the home screen

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,32 +1,24 @@
-import PropertyCard from '../../../components/PropertyCard';
-import properties from '../../../properties.json';
-
+import PropertyCard from "../../../components/PropertyCard";
+import properties from "../../../properties.json";
 
 const PropertiesPage = () => {
-    return (
-        <>
-        <section className='px-4 py-6'>
-            <div className="container-xl lg:container mx-auto px-4 py-6">
-                { properties.length === 0 ? (<p>No Property Found</p>) : (
-                    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                        {
-                            properties.map((property) => (
-                                // <div>{ property.name }</div>
-                                <PropertyCard key={property._id} property={property} />
-                            ))
-                        }
-                    </div>
-                ) }
+  return (
+    <>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container mx-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <p>No Property Found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
             </div>
-        </section>
-        </>
-    );
-}
- 
-export default PropertiesPage;
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
 
-// const PropertiesPage = () => {
-//     return ( <div>Property Page</div> );
-// }
- 
-// export default PropertiesPage;
+export default PropertiesPage;
