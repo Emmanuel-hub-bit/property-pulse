44430f16e1ab4c358d895032f71e4a175bc19bde
Emmanuel <emmanuelonyango467@gmail.com>
2025-05-06 13:00:50 +0300

    property add

--- utils/getSessionUser.ts
+++ utils/getSessionUser.ts
@@ -4,7 +4,18 @@ import { authOptions } from "./authOptions";
 export const getSessionUser = async () => {
     try {
         const session = await getServerSession(authOptions);
-    } catch (error) {
+
+        if(!session || !session.user) {
+            return null;
+        }
+
+        return {
+            user: session.user,
+            userId: session.user.id
+        }
         
+    } catch (error) {
+        console.error(error);
+        return null;
     }
 }
