e2f21378380abba2f9b96aa2f44673f175e6c9e0
Emmanuel <emmanuelonyango467@gmail.com>
2025-05-23 18:47:12 +0300

    search component

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -10,10 +10,59 @@ import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 import { covertToSerializableObject } from "../../../../utils/convertToObject";
 
-const PropertyPage = async ({ params }) => {
+// const PropertyPage = async ({ params }) => {
+//   await dbConnect();
+
+//   const propertyDoc = await Property.findById(params.id).lean();
+//   const property = covertToSerializableObject(propertyDoc);
+
+//   if (!property) {
+//     return (
+//       <h1 className="text-center text-2xl font-bold mt-10">
+//         Property Not Found
+//       </h1>
+//     );
+//   }
+
+//   return (
+//     <>
+//       <PropertyHeaderImage image={property.images[0][0]} />
+//       <section>
+//         <div className="container m-auto py-6 px-6">
+//           <Link
+//             href="/properties"
+//             className="text-blue-500 hover:text-blue-600 flex items-center"
+//           >
+//             <FaArrowLeft className="mr-2" /> Back to Properties
+//           </Link>
+//         </div>
+//       </section>
+//       <section className="bg-blue-50">
+//         <div className="container m-auto py-10 px-6">
+//           <div className="grid grid-cols-1 md:grid-cols-[3fr_1fr] w-full gap-6">
+//             <PropertyDetails property={property} />
+//             <aside className="space-y-4">
+//               <BookmarkButton property={property} />
+//               <ShareButtons property={property} />
+//               <PropertyContactForm property={property} />
+//             </aside>
+//           </div>
+//         </div>
+//       </section>
+//       <PropertyImages images={property.images} />
+//     </>
+//   );
+// };
+
+// export default PropertyPage;
+
+const PropertyPage = async (props) => {
   await dbConnect();
 
-  const propertyDoc = await Property.findById(params.id).lean();
+  const { params } = props;
+  const { id } = params;
+
+  const propertyDoc = await Property.findById(id).lean();
   const property = covertToSerializableObject(propertyDoc);
 
   if (!property) {
